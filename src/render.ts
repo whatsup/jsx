621a3d13bbd1be2df9115f919994dee00fbd3949
Denis Churbanov <4urbanoff@gmail.com>
2020-11-25 16:45:56 +0300

    update render

--- src/render.ts
+++ src/render.ts
@@ -1,22 +1,15 @@
-import { run, Streamable } from '@fract/core'
+import { run, Stream } from '@fract/core'
 import { reconcile, placeElements, removeUnreconciledElements } from './mutator'
 import { ReconcileMap } from './reconcile_map'
 import { FractalJSX } from './types'
 
-type Source = FractalJSX.Child | Streamable<Source>
-
-function* extract(source: Streamable<Source>): Generator<any, FractalJSX.Child, any> {
-    const result = yield* source
-    return result instanceof Streamable ? yield* extract(result) : result
-}
-
-export function render(source: Streamable<FractalJSX.Child>, container: HTMLElement | SVGElement = document.body) {
+export function render(source: Stream<FractalJSX.Child>, container: HTMLElement | SVGElement = document.body) {
     return run(function* () {
         try {
             let oldReconcileMap = new ReconcileMap()
 
             while (true) {
-                const result = yield* extract(source)
+                const result = yield* source
                 const children = Array.isArray(result) ? result : [result]
                 const elements = [] as (HTMLElement | SVGElement | Text)[]
                 const reconcileMap = new ReconcileMap()
